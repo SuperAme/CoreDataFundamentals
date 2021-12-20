d4038fe00b35cb1ecac75416c60bb41240650266
Americo Meneses <beatamez@hotmail.com>
2021-12-20 12:01:40 -0600

    firstCommit

---
+++ HitListCoreData/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  HitListCoreData
+//
+//  Created by Américo MQ on 20/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+    
+    func setupView() {
+        view.backgroundColor = .white
+    }
+
+
+}
+
