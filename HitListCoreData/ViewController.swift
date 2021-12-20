ad623e30927ddde63ede4d4469485bd29fca1678
Americo Meneses <beatamez@hotmail.com>
2021-12-20 12:52:16 -0600

    Read and insert in core data

--- HitListCoreData/ViewController.swift
+++ HitListCoreData/ViewController.swift
@@ -6,18 +6,109 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
+    
+    var people: [NSManagedObject] = []
+    
+    let mainTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
     }
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        mainTableView.frame = view.bounds
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Person")
+        
+        do {
+            people = try managedContext.fetch(fetchRequest)
+        } catch {
+            print("Error fetching data \(error)")
+        }
+    }
+    
     func setupView() {
+        view.addSubview(mainTableView)
+        
         view.backgroundColor = .white
+        
+        NSLayoutConstraint.activate([
+            mainTableView.topAnchor.constraint(equalTo: view.topAnchor),
+            mainTableView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            mainTableView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            mainTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        
+        mainTableView.dataSource = self
+        
+        let barBtn = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addName))
+        self.navigationItem.rightBarButtonItem = barBtn
+    }
+    
+    @objc func addName(_ sender: UIBarButtonItem) {
+        print("Im here")
+        let alert = UIAlertController(title: "New Name", message: "Add a new name", preferredStyle: .alert)
+        let saveAction = UIAlertAction(title: "Save", style: .default) { [unowned self] action in
+            guard let textField = alert.textFields?.first, let nameToSave = textField.text else { return }
+            self.save(name: nameToSave)
+            self.mainTableView.reloadData()
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default)
+        alert.addTextField()
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        present(alert, animated: true)
     }
 
+    func save(name: String) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        let entity = NSEntityDescription.entity(forEntityName: "Person", in: managedContext)!
+        
+        let person = NSManagedObject(entity: entity, insertInto: managedContext)
+        
+        person.setValue(name, forKey: "name")
+        
+        do {
+            try? managedContext.save()
+            people.append(person)
+        } catch {
+            print("Error saving data: \(error.localizedDescription)")
+        }
+    }
 
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return people.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let person = people[indexPath.row]
+        let cell = mainTableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        content.text = person.value(forKeyPath: "name") as? String
+        cell.contentConfiguration = content
+        return cell
+    }
 }
 
